712ae53642e3dfa3570ce9e4a3a4982553ece5ff
Strafred <bred7355608@gmail.com>
2023-04-23 10:02:33 +0700

    corretto

---
+++ public/worker.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");
+
+const remoteInitData = () => {
+  console.log("GAVNOOOOOOOOOOOOOOOOOOEDDDSSSSSSSSSSSSSSSSSSSSSSSSSIIIIIIIIIIII");
+};
+
+// eslint-disable-next-line no-undef
+Comlink.expose(remoteInitData);
